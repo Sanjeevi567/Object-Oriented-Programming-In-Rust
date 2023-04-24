58fa9f4c9c4c05116b7dbba322e6291e1677de80
Sanjeevi <sanjeevimani567@gmail.com>
2023-04-24 08:07:36 +0530

    Update getters.cpp

--- getters.cpp
+++ getters.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-class ADT {      
+string name = "Hello";
+int integer =10;
+class ADT {   
+ public:    
+    void print() { 
+        cout << "name is:" << name <<endl;
+        // we can even change the variable in outside.
+        integer =11;
+        cout <<"Integer is: "<<integer<<"\n";
+        }
+    void ddelete(){
+        cout<<"The string is: "<<name<<endl;
+        name.clear();
+    }
+    
   public:             
     int num;        
     string String;
@@ -22,6 +35,10 @@ class ADT {
 
 int main() {
   ADT obj; 
+     obj.print();
+     obj.ddelete();
+  cout<<"The string after the class method is: "<<name<<endl;     
+     
   cout <<"The int is " << obj.getInt() << "\n";
   cout <<"The string is " << obj.getString() << "\n";
   cout <<"The Bool is " << obj.getBool() << "\n";
